d1b8bd0c3b662b0b3d84a088b8a635a0344484a0
VISHNUPRASAD-K-G <110888065+VISHNUPRASAD-K-G@users.noreply.github.com>
2022-12-15 16:20:52 +0530

    files

--- files/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-csv=pd.read_csv("CSV.csv")
-print(csv)
-sr=csv.sort_values(["MARK"],ascending=False)
-print("\n  SORTED CSV FILE(Descending Order)")
-print("    ----------------------------\n\n",sr)
